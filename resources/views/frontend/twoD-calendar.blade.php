055806e4cd7bdc6e309996ee725921380bf90ee4
Thein Thein Soe <theintheinsoe@xyberplanet.com>
2023-12-02 10:14:04 +0630

    2d live, history, calendar ui fix

--- resources/views/frontend/twoD-calendar.blade.php
+++ resources/views/frontend/twoD-calendar.blade.php
@@ -4,56 +4,10 @@
     <div
       class="col-lg-4 col-md-4 offset-lg-4 offset-md-4 mt-4 pt-5 headers shadow"
       style="height:100vh;"
-    >    
+    >
       
-    <h2 class="text-center" style="color: #f5bd02">2D Calendar</h2>
-
-    <div class="row calendar-container">
-      <div class="col-md-3">
-        <div class="card">
-          <div class="card-header">January</div>
-          <div class="card-body">
-            <div class="day-cell">1</div>
-            <div class="day-cell">2</div>
-          </div>
-        </div>
-      </div>
-
-
-      <div class="col-md-3">
-        <div class="card">
-          <div class="card-header">February</div>
-          <div class="card-body">
-            
-            <div class="day-cell">1</div>
-            <div class="day-cell">2</div>
-            
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="card">
-          <div class="card-header">March</div>
-          <div class="card-body">
-            
-            <div class="day-cell">1</div>
-            <div class="day-cell">2</div>
-            
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="card">
-          <div class="card-header">April</div>
-          <div class="card-body">
-            
-            <div class="day-cell">1</div>
-            <div class="day-cell">2</div>
-            
-          </div>
-        </div>
-      </div>
-    </div>
+      <p class="text-center fs-4 fw-bold mt-3" style="color: #f5bd02">2D Calendar</p>
+      <div id="2dCalender"></div>
     </div>
 </div>
 
@@ -64,11 +18,11 @@
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script>
     async function fetchData() {
-      const url = 'https://shwe-2d-live-api.p.rapidapi.com/live';
+      const url = 'https://shwe-2d-live-api.p.rapidapi.com/calender';
       const options = {
         method: 'GET',
         headers: {
-          'X-RapidAPI-Key': '4c6bcd02e8msh0665010fc0fab0fp1a2d33jsn173e389166b3',
+          'X-RapidAPI-Key': '53aaa0f305msh5cdcf7afaacaedcp11a2d2jsn2453bc4f2507',
           'X-RapidAPI-Host': 'shwe-2d-live-api.p.rapidapi.com'
         }
       };
@@ -76,34 +30,66 @@
       try {
         const response = await fetch(url, options);
         const result = await response.json(); // Parse the response as JSON
+        const calendar = result.message;
+        // console.log(calendar);
 
+                let newHTML = '';
+                    calendar.forEach(r => {
+                        let originalString = r.update;
+                        let dateString = originalString.split(':')[1].trim().split(' ')[0];
+                        let dateObj = new Date(dateString.replace(/(\d{2})\/(\w{3})\/(\d{4})/, '$2 $1, $3'));
+                        let formattedDate = dateObj.toISOString().split('T')[0];
+                        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+                        let dayOfWeek = days[dateObj.getDay()];
+                        let updatedTime = `${dateString} (${dayOfWeek})`;
+                        // console.log(dateString);
 
-        // document.getElementById("two_d_live").innerText = result.live_result
-        $("#updated_time").text(result.update);
+                        newHTML += `
+                        <p class="text-center fw-bold custom-tables">${updatedTime}</p>
+                        <table class='table text-center mb-1 pb-'>
+                            <tr class='text-center'>
+                                <th>Time</th>
+                                <th>Internet</th>
+                                <th>Modern</th>
+                            </tr>
+                            <tr>
+                                <td>9:30AM</td>
+                                <td>${r.a9_internet}</td>
+                                <td>${r.a9_modern}</td>
+                            </tr>
+                            <tr>
+                                <td>2:00PM</td>
+                                <td>${r.a2_internet}</td>
+                                <td>${r.a2_modern}</td>
+                            </tr>
+                        </table>
+                        <table class="table table-transparent mt-0 pt-0">
+                            <tr class='text-center'>
+                                <th>Time</th>
+                                <th>BTC</th>
+                                <th>ETH</th>
+                                <th>2D</th>
+                            </tr>
+                            <tr>
+                                <td>12:00PM</td>
+                                <td>${r.a12_set}</td>
+                                <td>${r.a12_value}</td>
+                                <td>${r.a12_result}</td>
+                            </tr>
+                            <tr>
+                                <td>4:30PM</td>
+                                <td>${r.a43_set}</td>
+                                <td>${r.a43_value}</td>
+                                <td>${r.a43_result}</td>
+                            </tr>
+                        </table>
+                        `;
+                    });
+                $('#2dCalender').html(newHTML);
 
-        $("#two_d_live").text(result.live_result);
-        $("#live_result").text(result.live_result);
-        $("#live_set").text(result.live_set);
-        $("#live_value").text(result.live_value);
-
-        // $("#a9_result").text(result.a9_internet);
-        $("#a9_internet").text(result.a9_internet);
-        $("#a9_modern").text(result.a9_modern);
-
-        $("#a12_result").text(result.a12_result);
-        $("#a12_set").text(result.a12_set);
-        $("#a12_value").text(result.a12_value);
-
-        // $("#a2_result").text(result.a2_internet);
-        $("#a2_internet").text(result.a2_internet);
-        $("#a2_modern").text(result.a2_modern);
-
-        $("#a43_result").text(result.a43_result);
-        $("#a43_set").text(result.a43_set);
-        $("#a43_value").text(result.a43_value);
-        console.log(result);
+        console.log(calendar);
       } catch (error) {
-        console.error(error);
+        console.log(error);
       }
     }
     fetchData();
