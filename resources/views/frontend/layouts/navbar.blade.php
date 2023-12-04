e6ea93417648f0695357ba33b13301f76c259957
Thein Thein Soe <theintheinsoe@xyberplanet.com>
2023-12-04 19:19:00 +0630

    ui fix

--- resources/views/frontend/layouts/navbar.blade.php
+++ resources/views/frontend/layouts/navbar.blade.php
@@ -11,6 +11,6 @@
       </h5>
       <span>
           <button class="material-icons text-decoration-none btn" style="color:#f5bd02; border:none" onclick="refreshPage()"
-            >refresh</button>
+            >refresh</button>   
       </span>
   </div>
