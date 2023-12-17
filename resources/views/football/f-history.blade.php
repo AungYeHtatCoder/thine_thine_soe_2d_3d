d54fdf9bf8ad60f366231609579fa6a32aa2d7e1
kzt <d4all.digit4all.2013@gmail.com>
2023-12-18 02:00:13 +0700

    update

--- resources/views/football/f-history.blade.php
+++ resources/views/football/f-history.blade.php
@@ -43,6 +43,104 @@ if ($mmHistory != null) {
 ?>
 </div>
 </div>
+<div class="modal fade col-sm-4" id="historyPnl" tabindex="-1" role="dialog" aria-labelledby="bet" aria-hidden="true">
+               <div class="modal-dialog modal-dialog-centered" role="document">
+                  <div class="modal-content">
+                     <div class="modal-header">
+                        <h5 class="modal-title  text-dark" id="exampleModalLongTitle">လောင်းထားသောပွဲများ</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 
+                     </div>
+                     <div class="modal-body" id="divPreview">
+
+                     </div>
+                     <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">ပိတ်မည်</button>
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+
+            
 @include('frontend.layouts.footer')
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+<!-- Bootstrap JavaScript and dependencies -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script>
+            function mxDetail(v_id) {
+               $.ajax({
+                  headers: {
+                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                  },
+
+                  url: "{{  route('mp.detail') }}",
+                  type: 'GET',
+                  data: v_id,
+                  cache: false,
+                  dataType: 'json',
+
+                  success: function(data) {
+                     if(data.resCode == 200){
+                      alert(data.resDesc);
+                      mmOddBet = {};
+                      $('#divPreview').empty();
+                      $('#historyPnl').modal('show');
+                      location.reload();
+                     }else{
+                      alert(data.resDesc);
+                     }
+                  },
+                  error: function(data, ajaxOptions, thrownError) {
+                     var status = data.status;
+                     console.log(data);
+                     alert(status);
+                     if (data.status === 422) {
+                        $.each(data.responseJSON.errors, function(key, value) {
+                           console.log(value);
+                        });
+                     }
+                  }
+               });
+            }
+
+            function mmDetail(v_id) {
+               $.ajax({
+                  headers: {
+                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                  },
+
+                  url: "{{  route('mm.detail') }}",
+                  type: 'GET',
+                  data: v_id,
+                  cache: false,
+                  dataType: 'json',
+
+                  success: function(data) {
+                     if(data.resCode == 200){
+                      alert(data.resDesc);
+                      mmOddBet = {};
+                      $('#divPreview').empty();
+                      $('#historyPnl').modal('show');
+                      location.reload();
+                     }else{
+                      alert(data.resDesc);
+                     }
+                  },
+                  error: function(data, ajaxOptions, thrownError) {
+                     var status = data.status;
+                     console.log(data);
+                     alert(status);
+                     if (data.status === 422) {
+                        $.each(data.responseJSON.errors, function(key, value) {
+                           console.log(value);
+                        });
+                     }
+                  }
+               });
+            }
+            </script>
 @endsection
